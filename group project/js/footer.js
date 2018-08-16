b7af8099ec27eed08915c4ed1c38cdc1e19973fe
ssb91 <sasha.birdi@yahoo.co.uk>
2018-08-16 14:18:34 +0100

    upload

---
+++ group project/js/footer.js
@@ -0,0 +1,3 @@
+    $(function(){
+      $("#includeFooter").load("footer.html"); 
+    });
---
+++ group project/js/login.js
@@ -0,0 +1,39 @@
+// Variable to count number of attempts.
+var attempt = 3; 
+
+// Below function Executes on click of login button.
+function validate(){
+
+var username = document.getElementById("username").value;
+var password = document.getElementById("password").value;
+
+if ( username == "Formget" && password == "formget#123"){
+alert ("Login successfully");
+window.location = "success.html"; // Redirecting to other page.
+return false;
+}
+
+else{
+attempt --;// Decrementing by one.
+alert("You have left "+attempt+" attempt;");
+
+// Disabling fields after 3 attempts.
+
+if( attempt == 0){
+document.getElementById("username").disabled = true;
+document.getElementById("password").disabled = true;
+document.getElementById("submit").disabled = true;
+return false;
+}
+}
+}
+
+// Get the modal
+var modal = document.getElementById('clickaction');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
